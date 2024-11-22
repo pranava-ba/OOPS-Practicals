c9e96bb35fba2a474589ed60d0608ee62d7f3698
BA Pranava <97015345+pranava-ba@users.noreply.github.com>
2024-11-22 17:59:00 +0530

    Create Experiment5.java

---
+++ Experiment5.java
@@ -0,0 +1,44 @@
+interface ShapeTwo {
+    void printArea();
+}
+
+class RectangleTwo implements ShapeTwo {
+    private int length, breadth;
+    public RectangleTwo(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    public void printArea() {
+        System.out.println("Area of Rectangle: " + (length * breadth));
+    }
+}
+
+class TriangleTwo implements ShapeTwo {
+    private int base, height;
+    public TriangleTwo(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+    public void printArea() {
+        System.out.println("Area of Triangle: " + (0.5 * base * height));
+    }
+}
+
+class CircleTwo implements ShapeTwo {
+    private int radius;
+    public CircleTwo(int radius) {
+        this.radius = radius;
+    }
+    public void printArea() {
+        System.out.println("Area of Circle: " + (Math.PI * radius * radius));
+    }
+}
+
+ class ShapeMainTwo {
+    public static void main(String[] args) {
+        ShapeTwo[] shapes = { new RectangleTwo(5, 10), new TriangleTwo(4, 8), new CircleTwo(6) };
+        for (ShapeTwo shape : shapes) {
+            shape.printArea();
+        }
+    }
+}
